ae4d01819a540e8516b031ae5ceda279c40c5a98
moharrmmoharam-blip <moharrmmoharam@gmail.com>
2026-02-10 20:28:46 +0300

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,11 +1,1 @@
-bot/
-│
-├── main.py
-│
-├── handlers/
-│   ├── __init__.py        ✅ مهم جدًا
-│   ├── account_handlers.py
-│   ├── ad_handlers.py
-│   ├── admin_handlers.py
-│   ├── group_handlers.py
-│   └── reply_handlers.py
+
